2d735b2deef86bd9a5f2859142953741f219785e
elnaz-arab <arabelnaz33@gmail.com>
2021-10-29 02:38:11 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       int i, j, p;
+            for (i = 5; i > 0; i--)
+            {
+                for (p = 5; p > i; p--)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 0; j<i; j++)
+                {
+                    Console.Write("* ");
+                }
+               System.Console.WriteLine();
+            }
+        }
+    }
+}
